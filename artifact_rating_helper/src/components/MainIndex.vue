992f9d8af14cdf2eab2d927d111ffb792d8f1815
ynzhao <zhaoyn96@gmail.com>
2022-06-12 06:10:43 +0800

    创建工程

---
+++ artifact_rating_helper/src/components/MainIndex.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <span>xxxxxxxx</span>
+    <Button>xxxx</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from 'ant-design-vue'
+</script>
+
+<style scoped>
+
+</style>
